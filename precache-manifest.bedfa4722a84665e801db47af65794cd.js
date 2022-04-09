b5652ab09f07a2fba790c4a29b3a0ccd65f1c73e
Emily <brancocronos@hotmail.com>
2022-04-09 13:13:52 -0400

    Updates

---
+++ precache-manifest.bedfa4722a84665e801db47af65794cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "493aee0776a3320116a4",
+    "url": "/clone-netflix/static/css/main.11526272.chunk.css"
+  },
+  {
+    "revision": "493aee0776a3320116a4",
+    "url": "/clone-netflix/static/js/main.493aee07.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/clone-netflix/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "1a74aa8ff67eb060f4c4",
+    "url": "/clone-netflix/static/js/2.1a74aa8f.chunk.js"
+  },
+  {
+    "revision": "0fc1de498bf6ce906c1b",
+    "url": "/clone-netflix/static/js/runtime~main.0fc1de49.js"
+  },
+  {
+    "revision": "baa388bd42304843fa7751f6456068cb",
+    "url": "/clone-netflix/index.html"
+  }
+];
