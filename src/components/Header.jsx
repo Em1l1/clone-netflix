59a7116e85ff53f58dd8f9642013b5fa118f5f73
Emily <blancocronos@hotmail.com>
2022-09-01 16:16:11 -0600

    add header and footer 💖

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Header.css';
+
+export default ({black}) => {
+  return (
+    <header className={black ? 'black' : ''}>
+      <div className="header--logo">
+        <a href="/">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png" alt="Netflix" />
+        </a>
+      </div>
+      <div className="header--user">
+        <a href="/">
+          <img src="https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80" alt="usuario" />
+        </a>
+      </div>
+    </header>
+  );
+}
